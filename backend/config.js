2b15f6f8e655e96e64dad1b862a05b0572913bfa
ITboo <annaacdc@tut.by>
2023-09-01 21:07:41 +0300

    chore: add mongoose lib

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,6 @@
 export const PORT = 5555;
+
+//! UPDATE DB URL
+export const mongoDBURL =
+    'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
-import { PORT } from './config.js';
+import mongoose, { connect } from 'mongoose';
+import { PORT, mongoDBURL } from './config.js';
 
 const app = express();
-app.get('/', (request, response)=>{
-console.log(request);
-return response.status(200).send('Hello!');
+
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(200).send('Hello!');
 });
-app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`);
-})
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App successfully connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`);
+        })
+    })
+    .catch((error) => {
+        console.log(error);
+    })
