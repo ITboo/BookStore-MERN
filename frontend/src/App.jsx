de3400f4b920a5e52b3ee8b793242007e1447edc
ITboo <annaacdc@tut.by>
2023-09-02 22:37:25 +0300

    chore: add router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,19 @@
-import React from "react";
-
+//import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import DeleteBook from './pages/DeleteBook';
+import ShowBook from './pages/ShowBook';
+import UpdateBook from './pages/UpdateBook';
 const App = () => {
-return (
-  <div></div>
-)
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<UpdateBook />} />
+    </Routes>
+  )
 }
 export default App
