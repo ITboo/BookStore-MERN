7a9e3207619ca0f41076a39382e30e81ad9a8760
ITboo <annaacdc@tut.by>
2023-09-02 22:12:27 +0300

    init: start frontend development with Vite

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import mongoose, { connect } from 'mongoose';
+import cors from 'cors';
 import { PORT, mongoDBURL } from './config.js';
 import booksRoute from './routes/booksRoute.js';
 
 const app = express();
+
 app.use(express.json());
+app.use(
+    cors({
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        allowedHeaders: ['Content-Type'],
+    })
+);
 
 app.get('/', (request, response) => {
     console.log(request);
