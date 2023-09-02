054727428e92d5f7483d014b562abe648f998af5
ITboo <annaacdc@tut.by>
2023-09-02 21:52:27 +0300

    feat: add methods and router

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import mongoose, { connect } from 'mongoose';
 import { PORT, mongoDBURL } from './config.js';
+import booksRoute from './routes/booksRoute.js';
 
 const app = express();
+app.use(express.json());
 
 app.get('/', (request, response) => {
     console.log(request);
     return response.status(200).send('Hello!');
 });
+
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
